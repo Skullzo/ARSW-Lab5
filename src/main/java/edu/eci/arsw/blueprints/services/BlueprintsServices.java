697b5069b18aff038fefe09e49fe8508f44efc88
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-18 10:14:09 -0500

    Código Fuente

--- src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
+++ src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
@@ -4,14 +4,12 @@
  * and open the template in the editor.
  */
 package edu.eci.arsw.blueprints.services;
-
 import edu.eci.arsw.blueprints.filters.BlueprintsFilter;
 import edu.eci.arsw.blueprints.model.Blueprint;
 import edu.eci.arsw.blueprints.model.Point;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
-
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -19,34 +17,27 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-
 /**
  *
  * @author hcadavid
  */
 @Service("BlueprintsServices")
 public class BlueprintsServices {
-
     @Autowired
     @Qualifier("InMemoryBlueprintPersistence")
     BlueprintsPersistence bpp;
-
     @Autowired
     @Qualifier("RedundancyFilter")
     BlueprintsFilter bpf;
-
     public Blueprint filter(Blueprint bp){
         return bpf.filter(bp);
     }
-
     public void addNewBlueprint(Blueprint bp) throws BlueprintPersistenceException {
         bpp.saveBlueprint(bp);
-    }
-    
+    } 
     public Set<Blueprint> getAllBlueprints() throws BlueprintNotFoundException {
         return bpp.getAllBlueprints();
-    }
-    
+    }  
     /**
      * 
      * @param author blueprint's author
@@ -56,8 +47,7 @@ public class BlueprintsServices {
      */
     public Blueprint getBlueprint(String author,String name) throws BlueprintNotFoundException{
         return bpp.getBlueprint(author,name);
-    }
-    
+    }   
     /**
      * 
      * @param author blueprint's author
